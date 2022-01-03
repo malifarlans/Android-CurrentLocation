8929c06d8cb0740062a8b9e87d8f72d30554f097
Malifarlans <farlan.bkc@gmail.com>
2022-01-03 12:07:36 +0700

    first commit

---
+++ app/src/main/java/com/example/currentlocation/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.currentlocation
+
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    //CURRENTLOCATION
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProvider: FusedLocationProviderClient
+    private val permissionCode = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //CURRENTLOCATION
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
+
+    }
+    //CURRENTLOCATION
+    private fun fetchLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+
+        val task = fusedLocationProvider.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null){
+                currentLocation = location
+                Toast.makeText(this, currentLocation.latitude.toString() + "" + currentLocation.longitude.toString(), Toast.LENGTH_SHORT).show()
+
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.myMap) as
+                        SupportMapFragment?) !!
+                supportMapFragment.getMapAsync(this)
+            }
+        }
+    }
+    //CURRENTLOCATION
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        val makerOptions = MarkerOptions().position(latLng).title("Hello i am here")
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5f))
+        googleMap?.addMarker(makerOptions)
+    }
+    //CURRENTLOCATION
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            permissionCode -> if (grantResults.isEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                fetchLocation()
+            }
+        }
+    }
+}
